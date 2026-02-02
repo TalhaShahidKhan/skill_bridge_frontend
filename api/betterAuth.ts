5c4a95edb22185b1b31c1f8b57dbfbeef92e5774
Talha Shahid Khan <157911509+TalhaShahidKhan@users.noreply.github.com>
2026-02-03 00:41:12 +0600

    Added cookies fix

--- api/betterAuth.ts
+++ api/betterAuth.ts
@@ -3,6 +3,9 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_AUTH_URL || "http://localhost:8000/api/auth",
+  fetchOptions: {
+    credentials: "include",
+  },
   plugins: [
     inferAdditionalFields({
       user: {
